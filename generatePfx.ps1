c50e6f095ff75434244756163f9b8a9863652f76
Marc Denman <marc.denman@ukho.gov.uk>
2019-07-23 20:06:01 +0100

    Pass variable correctly

--- generatePfx.ps1
+++ generatePfx.ps1
@@ -18,7 +18,7 @@ if(-not(Test-Path env:$pfxPasswordVariableName)){
 $kvSecretBytes = [System.Convert]::FromBase64String("$pfxString")
 $certCollection = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2Collection 
 $certCollection.Import($kvSecretBytes,$null,[System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable)
-$protectedCertificateBytes = $certCollection.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Pkcs12, "$env:password")
+$protectedCertificateBytes = $certCollection.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Pkcs12, "$env:pfxPasswordVariableName")
 [System.IO.File]::WriteAllBytes("$pfxOutLocation", $protectedCertificateBytes)
 
 Write-Host
