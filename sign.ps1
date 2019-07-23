d171c82d07e8d7592a21486101ea37ddaadefe1b
Marc Denman <marc.denman@ukho.gov.uk>
2019-07-23 19:59:56 +0100

    Correct password

--- sign.ps1
+++ sign.ps1
@@ -6,4 +6,4 @@ $signtoolPath = "C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool\signto
 $dllLocation = "$env:Build_SourcesDirectory\src\bin\Debug\netcoreapp2.2\CodeSigning-Demo.dll"
 Write-Host "Trying to sign following DLL - $dllLocation"
 
-& $signtoolPath sign /f $pfxPath /p "$($env:pfxPassword)" /t "http://timestamp.verisign.com/scripts/timstamp.dll" "$dllLocation"
+& $signtoolPath sign /f $pfxPath /p $env:pfxPassword /t "http://timestamp.verisign.com/scripts/timstamp.dll" "$dllLocation"
