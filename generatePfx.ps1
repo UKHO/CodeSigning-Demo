e6483f14a9b762e3a1b6e504bfdd1c96892bd631
Marc Denman <marc.denman@ukho.gov.uk>
2019-07-23 20:44:39 +0100

    Use full fat env var

--- generatePfx.ps1
+++ generatePfx.ps1
@@ -15,12 +15,15 @@ if(-not(Test-Path env:$pfxPasswordVariableName)){
     exit 1
 }
 
+New-Item env:\$someVariable -Value "some kind of value"
+Get-Item env:\$var -Value
+
 gci env:
 
 $kvSecretBytes = [System.Convert]::FromBase64String("$pfxString")
 $certCollection = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2Collection 
 $certCollection.Import($kvSecretBytes,$null,[System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable)
-$protectedCertificateBytes = $certCollection.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Pkcs12, "${env:$pfxPasswordVariableName}")
+$protectedCertificateBytes = $certCollection.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Pkcs12, [Environment]::GetEnvironmentVariable($pfxPasswordVariableName))
 [System.IO.File]::WriteAllBytes("$pfxOutLocation", $protectedCertificateBytes)
 
 Write-Host
