622772ab6dd9b8ea92792338a828a6d4f04607a4
Marc Denman <marc.denman@ukho.gov.uk>
2019-07-23 17:09:08 +0100

    Move generate into script file

--- generatePfx.ps1
+++ generatePfx.ps1
@@ -1,9 +1,7 @@
-param($pfxString)
+param($pfxString, $pfxOutLocation)
 
-Write-Host $pfxString
-# # The certificate is downloaded from Key Vault as a string. Convert it into a PFX, password protect it and write to disk
-# $kvSecretBytes = [System.Convert]::FromBase64String('$(UKHOCodeSigningCert)')
-# $certCollection = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2Collection 
-# $certCollection.Import($kvSecretBytes,$null,[System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable)
-# $protectedCertificateBytes = $certCollection.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Pkcs12, "$(password)")
-# [System.IO.File]::WriteAllBytes("$(pfxLocation)", $protectedCertificateBytes)  
+$kvSecretBytes = [System.Convert]::FromBase64String("$pfxString")
+$certCollection = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2Collection 
+$certCollection.Import($kvSecretBytes,$null,[System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable)
+$protectedCertificateBytes = $certCollection.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Pkcs12, "$env:password")
+[System.IO.File]::WriteAllBytes("$pfxOutLocation", $protectedCertificateBytes)
