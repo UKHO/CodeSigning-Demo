ed051d1a7ab6d67c4e4806c9db874eb3886e97eb
Marc Denman <marc.denman@ukho.gov.uk>
2019-07-23 19:36:55 +0100

    Add missing envar

--- sign.ps1
+++ sign.ps1
@@ -5,4 +5,4 @@ param(
 $signtoolPath = "C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool\signtool.exe"
 $dllLocation = "$env:Build_SourcesDirectory\src\bin\Debug\netcoreapp2.2\CodeSigning-Demo.dll"
 Write-Host "Trying to sign following DLL - $dllLocation"
-& $signtoolPath sign /f $pfxPath /p $env:password /t "http://timestamp.verisign.com/scripts/timstamp.dll" $dllLocation
+& $signtoolPath sign /f $pfxPath /p $env:pfxPassword /t "http://timestamp.verisign.com/scripts/timstamp.dll" $dllLocation
