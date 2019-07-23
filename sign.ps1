c4445573310027c66365fe2b47e50e06f89cfd4c
Marc Denman <marc.denman@ukho.gov.uk>
2019-07-23 20:02:31 +0100

    debug wrong password error

--- sign.ps1
+++ sign.ps1
@@ -5,5 +5,6 @@ param(
 $signtoolPath = "C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool\signtool.exe"
 $dllLocation = "$env:Build_SourcesDirectory\src\bin\Debug\netcoreapp2.2\CodeSigning-Demo.dll"
 Write-Host "Trying to sign following DLL - $dllLocation"
+Write-Host "PFX path - $pfxPath"
 
 & $signtoolPath sign /f $pfxPath /p $env:pfxPassword /t "http://timestamp.verisign.com/scripts/timstamp.dll" "$dllLocation"
