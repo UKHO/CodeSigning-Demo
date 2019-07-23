e3cd02d3b887195c47bf1b12e4d369462acf004a
Marc Denman <marc.denman@ukho.gov.uk>
2019-07-23 20:17:15 +0100

    test

--- generatePfx.ps1
+++ generatePfx.ps1
@@ -15,6 +15,8 @@ if(-not(Test-Path env:$pfxPasswordVariableName)){
     exit 1
 }
 
+gci env:
+
 $kvSecretBytes = [System.Convert]::FromBase64String("$pfxString")
 $certCollection = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2Collection 
 $certCollection.Import($kvSecretBytes,$null,[System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable)
