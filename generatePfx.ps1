e2ee961de1ba3ab76ea831b1c408347314a10b6e
Marc Denman <marc.denman@ukho.gov.uk>
2019-07-23 17:51:02 +0100

    Refactor variables

--- generatePfx.ps1
+++ generatePfx.ps1
@@ -1,8 +1,14 @@
 param
 (
     $pfxString,
-    $pfxPasswordVariableName = "Password" # See for details on why not passing string - https://docs.microsoft.com/en-us/azure/devops/pipelines/process/variables?view=azure-devops&tabs=yaml%2Cbatch#secret-variables
+    $pfxPasswordVariableName = "Password", # See for details on why not passing string - https://docs.microsoft.com/en-us/azure/devops/pipelines/process/variables?view=azure-devops&tabs=yaml%2Cbatch#secret-variables
+    $pfxOutLocation = "$env:Build_SourcesDirectory\CodeSignCert.pfx"
 )
+
+#SetBetterDefault for password
+
+Write-Host "`$pfxPasswordVariableName set to $pfxPasswordVariableName"
+Write-Host "`$pfxOutLocation` set to $pfxOutLocation"
 
 if(-not(Test-Path env:$pfxPasswordVariableName)){
     $errorMessage = "$pfxPasswordVariableName does not exist as an environment variable. This needs to be set and contain the intended password for PFX"
@@ -10,13 +16,6 @@ if(-not(Test-Path env:$pfxPasswordVariableName)){
     Write-Host "##vso[task.logissue type=error]$errorMessage"
     exit 1
 }
-
-# Check existence of password
-# Set outlocation
-# Set password
-
-$pfxOutLocation = "$env:Build_SourcesDirectory\CodeSignCert.pfx"
-Write-Host "PFX will be written to $pfxOutLocation"
 
 $kvSecretBytes = [System.Convert]::FromBase64String("$pfxString")
 $certCollection = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2Collection 
